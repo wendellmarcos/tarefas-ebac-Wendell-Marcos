58675f99c11c40b7e59cc81403dd4207d345cf2a
Wendell Marcos <wendell.marcos.code@gmail.com>
2022-09-03 01:13:00 -0300

    Adicionando meu nome que faltou

--- PrimeiraClasse.java
+++ PrimeiraClasse.java
@@ -3,7 +3,7 @@ package br.com.wmw;
 public class PrimeiraClasse {
 
 	public static void main(String[] args) {
-		System.out.print("Hello World!");
+		System.out.print("Hello Wendell Marcos!");
 	}
 
 }
